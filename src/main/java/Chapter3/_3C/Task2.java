a93aae31c2c519bffc91b3d111fa29f511928afe
GyeomFka <clazziquai01@gmail.com>
2021-12-03 14:45:42 +0900

    z/b task

--- src/main/java/Chapter3/_3C/Task2.java
+++ src/main/java/Chapter3/_3C/Task2.java
@@ -1,4 +1,49 @@
 package Chapter3._3C;
 
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        final String MENU =
+                "시작화면"
+                + System.lineSeparator()
+                + System.lineSeparator() + "[메뉴선택]"
+                + System.lineSeparator() + "1. 회원 관리"
+                + System.lineSeparator() + "2. 과목 관리"
+                + System.lineSeparator() + "3. 수강 관리"
+                + System.lineSeparator() + "4. 결제 관리"
+                + System.lineSeparator() + "5. 종료"
+                + System.lineSeparator() + "입력 → ";
+
+        boolean isContinue = true;
+
+        while( isContinue ) {
+
+            System.out.print(MENU);
+            int caseNumber = scanner.nextInt();
+
+            switch( caseNumber ) {
+                case 1 :
+                    System.out.println("회원 관리 메뉴를 선택했습니다.");
+                    break;
+                case 2 :
+                    System.out.println("과목 관리 메뉴를 선택했습니다.");
+                    break;
+                case 3 :
+                    System.out.println("수강 관리 메뉴를 선택했습니다.");
+                    break;
+                case 4 :
+                    System.out.println("결제 관리 메뉴를 선택했습니다.");
+                    break;
+                case 5 :
+                    System.out.println("프로그램을 종료합니다.");
+                    isContinue = false;
+                    break;
+            }
+        }
+    }
 }
--- src/main/java/Chapter3/_3C/Task3.java
+++ src/main/java/Chapter3/_3C/Task3.java
@@ -1,4 +1,41 @@
 package Chapter3._3C;
 
 public class Task3 {
+
+    public static void main(String[] args) {
+//        final String INAUGURAL_SPEECH = "show me the money, food for thought, power overwhelming. artist baby. life is short, artist work nunu.";
+        final String INAUGURAL_SPEECH = "aAd vfac cdDd aBbgzca bed aCbd baccd ";
+        final int stringLength = INAUGURAL_SPEECH.length();
+
+        String upperStr = INAUGURAL_SPEECH.toUpperCase();
+
+        int aCnt = 0;
+        int bCnt = 0;
+        int cCnt = 0;
+        int dCnt = 0;
+
+        for(int i = 0; i < stringLength; i++ ) {
+            switch ( upperStr.charAt(i) ) {
+                case 'A' :
+                    aCnt++;
+                    break;
+                case 'B' :
+                    bCnt++;
+                    break;
+                case 'C' :
+                    cCnt++;
+                    break;
+                case 'D' :
+                    dCnt++;
+                    break;
+            }
+        }
+
+        System.out.println("A : " + aCnt);
+        System.out.println("B : " + bCnt);
+        System.out.println("C : " + cCnt);
+        System.out.println("D : " + dCnt);
+
+    }
+
 }
